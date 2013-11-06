e9ed7e2aff5be8483d284387d90d6657af801f0b
Deniska <den92n@gmail.com>
2013-11-06 21:03:23 +0400

    commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemmoApp::Application.config.secret_token = 'a6e3879aa68e727230289cd7ed44746855ee087b40a9180a5490f3a05132b3523b6bdfea921cb09dab10926590ee733398712c50e5fc37667bbaa80d032f8b1a'
